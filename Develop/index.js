2175694d1a6d071dfbf501e2daab94cce6f555eb
Marvin Ren <marvin.ren@yale.edu>
2021-10-08 00:15:01 -0400

    question

---
+++ Develop/index.js
@@ -0,0 +1,27 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs')
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your Project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project'
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
